22b046c9e45b536527b6fd2577b2f8cd87ace96c
HDQ <3535768174@qq.com>
2024-11-12 21:59:55 +0800

    redemption

--- regfile/regfile.v
+++ regfile/regfile.v
@@ -1,20 +1,46 @@
 module regfile(
-  input         clk,
-  input  	    raddr1,
-  output        rdata1,
-  input         raddr2,
-  output        rdata2,
-  input         we,
-  input         waddr,
-  input         wdata
-);
-reg [31:0] rf[31:0];
-// WRITE
-always @(posedge clk) begin
+    input clk,
+    input [4:0] raddr1,
+    input [4:0] raddr2,
+    input we,
+    input [4:0] waddr,
+    input [31:0] wdata,
+    output reg [31:0] rdata1,
+    output reg [31:0] rdata2
+    );
+    reg [31:0] rf [31:0];
+    
 
-end
-// READ OUT 1
-assign rdata1 = 
-// READ OUT 2
-assign rdata2 = 
+    always @(posedge clk) begin
+        if (raddr1 == {5{1'b0}}) begin
+            rdata1 <= {32{1'b0}};
+        end
+        else begin
+            rdata1 <= rf[raddr1];
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (raddr2 == {5{1'b0}}) begin
+            rdata2 <= {32{1'b0}};
+        end
+        else begin
+            rdata2 <= rf[raddr2];
+        end
+    end
+    
+    always @(posedge clk) begin
+        if (we == 1'b1 ) begin
+            if (waddr == {5{1'b0}}) begin
+                rf[0] <= {32{1'b0}};
+            end
+            else begin
+                rf[waddr] <= wdata;       
+            end
+        end
+        else begin
+            rf[0] <= {32{1'b0}};
+        end
+    end
+    
 endmodule
